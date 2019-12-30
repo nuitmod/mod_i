b9eb936ee2d061a2af7d220cdc075867fcd7b95b
Пользователь <USER@Polzovatels-Mac-mini.local>
2019-12-30 14:29:39 +0500

    change idex js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import './news-article.js';
 import { topHeadlinesUrl } from './newsApi.js';
 
 window.addEventListener('load', () => {
-//  getNews();
+  //getNews();
   registerSW();
 });
 
@@ -11,7 +11,7 @@ async function getNews() {
   const json = await res.json();
 
   const main = document.querySelector('main');
-  
+
   /*
 
   json.articles.forEach(article => {
@@ -19,8 +19,9 @@ async function getNews() {
     el.article = article;
     main.appendChild(el);
   });
+  */
 }
-*/
+
 async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
