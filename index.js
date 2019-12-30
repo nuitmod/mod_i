7f0967468d0dacb74ddc47b0f318b20847cba8a8
Пользователь <USER@Polzovatels-Mac-mini.local>
2019-12-30 13:34:10 +0500

    rm forEach

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import './news-article.js';
 import { topHeadlinesUrl } from './newsApi.js';
 
 window.addEventListener('load', () => {
-  getNews();
+//  getNews();
   registerSW();
 });
 
@@ -11,6 +11,8 @@ async function getNews() {
   const json = await res.json();
 
   const main = document.querySelector('main');
+  
+  /*
 
   json.articles.forEach(article => {
     const el = document.createElement('news-article');
@@ -18,7 +20,7 @@ async function getNews() {
     main.appendChild(el);
   });
 }
-
+*/
 async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
