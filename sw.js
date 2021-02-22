a03e838b688960621756418566cff4b693fc9175
nuitmod <wtvfqm@gmail.com>
2021-02-22 19:38:55 +0500

    add amp

--- sw.js
+++ sw.js
@@ -46,3 +46,16 @@ async function networkAndCache(req) {
     return cached;
   }
 }
+//
+// importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+//
+// AMP_SW.init({
+//   assetCachingOptions: [{
+//     regexp: /\.(png|jpg|woff2|woff|css|js)/,
+//     cachingStrategy: 'CACHE_FIRST',
+//   }],
+//   offlinePageOptions: {
+//     url: '/offline.html',
+//     assets: [],
+//   },
+// });
